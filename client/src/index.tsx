40082e10e594bf47d5bfd6870a682a4ef7c4c25c
Haider Ali Khan <hk9675181@gmail.com>
2025-09-21 22:33:42 +0500

    Fix package vulnerabilities, setup husky, and fix CI workflows

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ThemedApp from './ThemedApp';
+import ThemeApp from './ThemeApp';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <ThemedApp />
+  <ThemeApp />
   </React.StrictMode>
 );
